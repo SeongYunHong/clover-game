54112744f2b761d02b5955b5032dc907dfaa0341
Hong <yundev5338@gmail.com>
2025-09-23 10:44:25 +0900

    tailwindcss 버전 변경(4.1.13 -> 3.4.17)

---
+++ tailwind.config.cjs
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx,vue,svelte}",
+    "./templates/**/*.{html,twig}",
+    "./**/*.{html,js}"
+  ],
+  theme: {
+    extend: {
+    },
+  },
+  plugins: [
+  ],
+}
